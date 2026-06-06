602ea30ff0ccf196346bf664444ef4ce58c9418f
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2026-06-06 18:20:32 -0400

    Update Home.py

--- frontend/Home.py
+++ frontend/Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # from utils.matches import format_date_local, format_kickoff, load_local_fixtures, split_local_fixtures
-from utils.ui import inject_base_styles, render_copyright_footer
+# from utils.ui import inject_base_styles, render_copyright_footer
 
 st.set_page_config(
     page_title="FIFA World Cup 2026",
